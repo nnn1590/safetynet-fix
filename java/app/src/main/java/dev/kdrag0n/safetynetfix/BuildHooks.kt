943ba134e39bcacb46f7218a2bc5ccd73bf9809f
NNN1590 <nnn1590@nnn1590.org>
2023-02-09 14:44:38 +0900

    Merge v2.3.1-MOD_3.0
    But changed
    	Build.DEVICE_INITIAL_SDK_INT
    to
    	Build.VERSION.DEVICE_INITIAL_SDK_INT

--- java/app/src/main/java/dev/kdrag0n/safetynetfix/BuildHooks.kt
+++ java/app/src/main/java/dev/kdrag0n/safetynetfix/BuildHooks.kt
@@ -4,14 +4,25 @@ import android.os.Build
 
 internal object BuildHooks {
     fun init() {
-        // Append a space to the device model name
         Build::class.java.getDeclaredField("MODEL").let { field ->
             field.isAccessible = true
-            field.set(null, Build.MODEL + " ")
+            field.set(null, "Pixel XL")
+        }
+        Build::class.java.getDeclaredField("DEVICE").let { field ->
+            field.isAccessible = true
+            field.set(null, "marlin")
+        }
+        Build::class.java.getDeclaredField("PRODUCT").let { field ->
+            field.isAccessible = true
+            field.set(null, "marlin")
         }
         Build::class.java.getDeclaredField("FINGERPRINT").let { field ->
             field.isAccessible = true
-            field.set(null, "google/angler/angler:6.0/MDB08L/2343525:user/release-keys")
+            field.set(null, "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys")
+        }
+        Build.VERSION::class.java.getDeclaredField("DEVICE_INITIAL_SDK_INT").let { field ->
+            field.isAccessible = true
+            field.set(null, Build.VERSION_CODES.N_MR1)
         }
     }
 }
